cb478c80f23b4524995cb54e21bd966fe6dd6f30
Ian Reynolds <idreyn@gmail.com>
2022-07-18 11:03:49 -0400

    some zustand-idiomatic improvements to useFacetsQuery

--- client/utils/useFacets.ts
+++ client/utils/useFacets.ts
@@ -1,6 +1,6 @@
-import { useContext, useMemo } from 'react';
+import { useCallback, useContext, useMemo, useRef } from 'react';
 
-import { FacetsContext, FacetState } from 'components';
+import { FacetsContext, FacetsState, FacetState } from 'components';
 import { CascadedFacetType, IntrinsicFacetName, Intrinsics } from 'facets';
 
 const throwFacetsStateError = (): never => {
@@ -15,11 +15,21 @@ export const useFacetsState = () => {
 	return actuallyUseFacets!();
 };
 
-type FacetsQueryLevel = 'current' | 'persisted' | 'latest';
+type FacetsQueryLevel =
+	// The state of the facet that the server has
+	| 'persisted'
+	// The latest _validated_ client-side version of the facet
+	| 'current'
+	// The latest client-side version of the facet, possibly including invalid props
+	| 'latest';
+
 type FacetsQueryable = { [K in keyof Intrinsics]: CascadedFacetType<Intrinsics[K]> };
 
 type UseFacetsQueryOptions<T> = {
+	// Return a value if the FacetsStateProvider is not available.
 	fallback?: () => T;
+	// Which version of the facet we want to look at
+	// Most consumers will want 'current' unless they deal with editing facet state.
 	level?: FacetsQueryLevel;
 };
 
@@ -40,6 +50,11 @@ export const useFacetsQuery = <T>(
 ): T => {
 	const { fallback, level = 'current' } = options;
 	const actuallyUseFacets = useContext(FacetsContext);
+
+	// This is a kind of silly escape hatch to plumb values from pickFromStore() to queryProxy.
+	// It lets us cache the queryProxy Proxy object instead of frequently creating new ones.
+	const latestState = useRef<null | FacetsState>(null);
+
 	if (!actuallyUseFacets) {
 		if (fallback) {
 			return fallback();
@@ -47,19 +62,31 @@ export const useFacetsQuery = <T>(
 		throwFacetsStateError();
 	}
 
-	const { facets } = actuallyUseFacets!();
-
+	// This appears to violate the Rules of Hooks by calling useMemo conditionally, but for the
+	// lifetime of the hook, actuallyUseFacets will never change, so we'll either always call
+	// this hook, or never call it.
 	// eslint-disable-next-line react-hooks/rules-of-hooks
-	const queryable = useMemo(
+	const queryProxy = useMemo(
 		() =>
 			new Proxy({} as FacetsQueryable, {
 				get: (_, facetName: IntrinsicFacetName) => {
-					const facetState = facets[facetName];
+					const facetState = latestState.current!.facets[facetName];
 					return getQueryableObjectForLevel(facetState, level).value;
 				},
 			}),
-		[facets, level],
+		[level],
 	);
 
-	return query(queryable);
+	// (Same as above)
+	// eslint-disable-next-line react-hooks/rules-of-hooks
+	const pickFromStore = useCallback(
+		(state: FacetsState) => {
+			latestState.current = state;
+			const queriedState = query(queryProxy);
+			return queriedState;
+		},
+		[queryProxy, query],
+	);
+
+	return actuallyUseFacets!(pickFromStore);
 };

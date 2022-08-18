93ed3230b91c1722cd8e7803fd4a184aeb380eb7
Ian Reynolds <idreyn@gmail.com>
2022-08-18 17:47:51 -0400

    don't even touch localDatabase module in production

--- init.js
+++ init.js
@@ -4,20 +4,18 @@ const {
 } = require('yargs');
 const path = require('path');
 
-const { setupLocalDatabase } = require('./localDatabase');
-
 const watchables = watch && (Array.isArray(watch) ? watch : [watch]).filter((x) => x);
 
 const main = async () => {
 	if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
 		require(path.join(process.cwd(), 'config.js'));
+		const { setupLocalDatabase } = require('./localDatabase');
+		await setupLocalDatabase();
 	}
 
 	if (process.env.NODE_ENV === 'production') {
 		require('newrelic');
 	}
-
-	await setupLocalDatabase();
 
 	const loadServer = () => {
 		return require('./dist/server/server/server').startServer();

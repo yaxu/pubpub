9ef3436791e19a5a2412f6ed6d0dc568c3da187c
qweliant <dmtorcode@gmail.com>
2023-07-17 17:12:52 -0400

    find math nodes

--- client/utils/suggestedEdits.ts
+++ client/utils/suggestedEdits.ts
@@ -4,11 +4,10 @@ export const hasMathNode = (editorChangeObject: EditorChangeObject | null): bool
 	if (!editorChangeObject || !editorChangeObject.view) return false;
 	const doc = editorChangeObject.view.state.doc;
 	let hasMath = false;
-	doc.nodesBetween(1, doc.nodeSize - 2, (node) => {
+	doc.nodesBetween(0, doc.nodeSize - 2, (node) => {
 		if (hasMath) return;
-		console.log(node);
 		// replace with function to check for math
-		const present = false;
+		const present = node.type.name === 'math_inline' || 'math_display';
 		if (present) hasMath = true;
 	});
 	return hasMath;
